67d42eae9be565fa625244d32ee7b07079dad3a8
Android789515 <derflug@mailfence.com>
2025-05-16 16:08:07 -0500

    feat: random word util
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/util.rs
@@ -0,0 +1,32 @@
+use std::{fs, path::Path};
+
+pub fn select_random_word() -> String {
+    let words_file = Path::new("./src/words.txt");
+
+    let mut rng = rand::rng();
+    let random_index = rand::Rng::random_range(&mut rng, 0..999);
+
+    let selected_word = fs::read_to_string(words_file)
+        .unwrap()
+        .split('\n')
+        .enumerate()
+        .fold(String::new(), |selected_word, (index, current_word)| {
+            if index == random_index {
+                String::from(current_word)
+            } else {
+                selected_word
+            }
+        });
+
+    selected_word
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn selects_random_word() {
+        assert!(!select_random_word().is_empty());
+    }
+}
